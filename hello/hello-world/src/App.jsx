725791f16d331ebfe188236ff2e98e96ead76855
siva-630 <siva630029@gmail.com>
2026-03-10 10:59:26 +0530

    hello-world-f

---
+++ hello/hello-world/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-amber-500'>
+      <h1 className="pt-10 ml-2 text-6xl text-center mt-[200px]">Hello siva</h1>
+    </div>
+  )
+}
+
+export default App
